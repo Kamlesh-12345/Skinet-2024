caea41cfcd7944faad305f5087a6f4b10b12de48
Kamlesh Lassi <lassi.kamlesh27@gmail.com>
2026-01-07 20:12:17 +0530

    End of section 6

---
+++ API/Errors/ApiErrorResponse.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.Build.Framework;
+
+namespace API.Errors;
+
+public class ApiErrorResponse(int statusCode, string message, string? details)
+{
+    public int StatusCode { get; set; } = statusCode;
+    public string Message { get; set; } = message;
+    public string? Details { get; set; } = details;
+}
